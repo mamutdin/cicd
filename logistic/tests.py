91bea2f6280f557936f4ef2bccd4f3cbf26fa21a
mamutdin <mamyt1993@mail.ru>
2022-11-13 02:28:21 +0300

    tests

--- logistic/tests.py
+++ logistic/tests.py
@@ -4,5 +4,5 @@ from django.test import TestCase, Client
 class TestMyCase(TestCase):
     def test_view(self):
         client = Client()
-        response = client.get('api/v1/stocks/')
+        response = client.get('http://127.0.0.1:8000/api/v1/stocks/')
         self.assertEqual(response.status_code, 200)
